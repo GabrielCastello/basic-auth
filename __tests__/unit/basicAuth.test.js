7966c10978c7a7c2b3110e64c2a7e7d4036a7270
Gabriel Castello <gabriel__castello@hotmail.com>
2020-01-27 14:28:30 -0300

    added jest tests with supertest, express and faker.

---
+++ __tests__/unit/basicAuth.test.js
@@ -0,0 +1,47 @@
+const BasicAuth = require("../../src/app");
+
+const request = require("supertest");
+const express = require("express");
+
+const faker = require("faker");
+const basicAuthCredentials = {
+  username: faker.internet.userName(),
+  password: faker.internet.password()
+};
+
+const wrongCredentials = {
+  username: faker.internet.userName(),
+  password: faker.internet.password()
+};
+const newBasicAuth = new BasicAuth(
+  basicAuthCredentials.username,
+  basicAuthCredentials.password
+);
+
+const app = express();
+app.get("/", newBasicAuth.protect, (req, res) => res.send("Hello World!"));
+
+describe("BasicAuth unit tests", () => {
+  it("should provide auth credentials and succefully returns 200", async () => {
+    const response = await request(app)
+      .get("/")
+      .auth(basicAuthCredentials.username, basicAuthCredentials.password);
+
+    expect(response.status).toBe(200);
+  });
+
+  it("should provide wrong auth credentials and returns 401", async () => {
+    const response = await request(app)
+      .get("/")
+      .auth(wrongCredentials.username, wrongCredentials.password);
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should not provide auth credentials and returns 401", async () => {
+    const response = await request(app)
+      .get("/")
+     
+    expect(response.status).toBe(401);
+  });
+});
